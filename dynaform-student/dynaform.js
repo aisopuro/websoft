42d6420ef7dc09933dd77d07a92e243e89a49c0f
aisopuro <atte.isopuro@aalto.fi>
2013-12-09 14:34:22 +0200

    First commit

---
+++ dynaform-student/dynaform.js
@@ -0,0 +1,34 @@
+//write your JavaScript here
+jQuery(document).ready(function() {
+    jQuery("#continent").change(function(event) {
+        var continent = jQuery(":selected").attr("value");
+        console.log(continent);
+        if (continent !== "clear") {
+            var address = "http://www2.cs.hut.fi/~petri/countrydata/";
+            address += continent + ".json?callback=?";
+            console.log(address);
+            var countries = jQuery("#country");
+            countries.empty();
+            countries.append("<option value=\"clear\">-- choose country --</option>");
+            jQuery.getJSON(address, function(data) {
+                // Ajax request successful
+                console.log("Got:");
+                console.log(data);
+                var option;
+                jQuery.each(data, function(val, content) {
+                    option = jQuery("<option></option>");
+                    option.attr("value", val);
+                    option.append(content);
+                    countries.append(option);
+                });
+            })
+                .done(function() {
+                    //done
+                })
+                .fail(function(jqxhr, textStatus, error) {
+                    console.log("error:");
+                    console.log(jqxhr);
+                });
+        }
+    });
+});
